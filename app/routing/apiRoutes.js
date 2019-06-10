15e718720db1701bcf49dde8c4b946c45de407bd
Paul Black <pblack67@comcast.net>
2019-06-10 09:03:12 -0500

    Added borders and border coloring to components. Added friend to friend list on submit.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,6 +17,7 @@ module.exports = function (app) {
                 bestFriend = friendData[i];
             }
         }
+        friendData.push(friend);
         response.json(bestFriend);
     });
 
